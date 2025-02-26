48a383678572cb7fc3b6a7ee68396a4b93bed3a2
makida-suzuka <s-makida@jinso.co.jp>
2025-02-26 17:48:58 +0900

    [fix]ハンバーガーメニュー fix

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ window.onload = function() {
     document.getElementById("hamburger").addEventListener("click", function () {
         // ハンバーガーメニューのクラスをトグル
         document.querySelector(".menu-right").classList.toggle("open");
+        //　背景
+        document.querySelector(".ham-back").classList.toggle("open");
+
         // ハンバーガーのクロス用クラスをトグル
         document.body.classList.toggle("hamburger-active");
       });
